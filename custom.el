0c329aa6d7ac859daf8c80f2d016d072d0625d94
Inigo Gonzalez de Arrieta <inigo.gonzalezdearrieta@ehu.eus>
2026-02-18 18:05:43 +0100

    Customize filled up custom.el

--- custom.el
+++ custom.el
@@ -1,2 +1,14 @@
 ;; Custom file to be used by Customize.
 ;; No user-generated code here.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(auctex blacken company-jedi markdown-mode pyvenv)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
